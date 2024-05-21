5ecdb9c7a891fe8877a4d7ee69f30fe431ddb71c
Kexicake <slava.kuznetsov.2017@gmail.com>
2024-05-21 23:36:54 +0400

    перевод хелп

--- myshell.c
+++ myshell.c
@@ -5,16 +5,16 @@
 
 // Функция для обработки команды help
 void execute_help() {
-    printf("myshell - A simple shell implementation.\n");
-    printf("Supported commands:\n");
-    printf("cd <directory> - Change directory to <directory>.\n");
-    printf("clr - Clear the screen.\n");
-    printf("dir <directory> - List the contents of <directory>.\n");
-    printf("environ - List all environment variables.\n");
-    printf("echo <comment> - Print <comment> to the screen.\n");
-    printf("help - Display this help message.\n");
-    printf("pause - Pause the shell until Enter is pressed.\n");
-    printf("quit - Exit the shell.\n");
+    printf("myshell - Простая реализация оболочки.\n");
+    printf("Поддерживаемые команды:\n");
+    printf("cd <directory> - Сменить каталог на <directory>.\n");
+    printf("clr - Очистить экран.\n");
+    printf("dir <directory> - Вывести содержимое каталога <directory>.\n");
+    printf("environ - Вывести все переменные среды.\n");
+    printf("echo <comment> - Вывести <comment> на экран.\n");
+    printf("help - Показать это сообщение с подсказками.\n");
+    printf("pause - Приостановить работу оболочки до нажатия Enter.\n");
+    printf("quit - Выйти из оболочки.\n");
 }
 
 int main(int argc, char *argv[]){
