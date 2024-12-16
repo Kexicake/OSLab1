21fc957e32354cc30b1824a4a983621238fbfc66
Kexicake <slava.kuznetsov.2017@gmail.com>
2024-12-16 11:40:06 +0400

    реализация потоков

--- myshell.c
+++ myshell.c
@@ -1,173 +1,303 @@
-#include <stdio.h> // perror , fgets
-#include <stdlib.h> // waitpid
-#include <string.h> // strcspn, strtok, strcmp
-#include <unistd.h> // системные вызовы: getcwd,fork,execvp,chdir
-#include <unistd.h>
-#include <string.h>
-#include <dirent.h>
-#include <sys/wait.h>
-#include <sys/types.h>
-#include <pwd.h>
-#include <errno.h>
 #include "myshell.h"
 
-// Объявление переменных среды
-extern char **environ;
+// Массив команд оболочки
+char *commands[] = {
+    "cd",      // смена директории
+    "clr",     // очистка экрана
+    "dir",     // просмотр содержимого директории
+    "environ", // вывод всех переменных окружения
+    "echo",    // вывод текста
+    "help",    // вывод справки
+    "pause",   // пауза, ожидание нажатия Enter
+    "quit"     // выход из оболочки
+};
 
-// Функция для вывода приглашения
-void print_prompt() {
-    char cwd[1024];
-    // Получаем текущий рабочий каталог и выводим его как приглашение
-    if (getcwd(cwd, sizeof(cwd)) != NULL) {
-        printf("%s> ", cwd);
-    } else {
-        perror("getcwd() error"); // Обрабатываем ошибку, если не удалось получить текущий каталог
-    }
+// Массив описаний команд
+char *command_descriptions[] = {
+    "cd <directory> - change the current default directory to <directory>. If the <directory> argument is missing, print the current directory.",
+    "clr - clearing the screen.",
+    "dir <directory> - displays the contents of the <directory> directory.",
+    "environ - output of all environment variables.",
+    "echo <comment> - output to the <comment> screen, after which the transition to a new line is performed.",
+    "help - output of the user's guide.",
+    "pause - suspends shell operations until the <Enter> key is pressed.",
+    "quit - exit the shell."
+};
+
+// Массив указателей на функции, соответствующие встроенным командам
+int (*command_functions[])(char **, char *, char *, int) = {
+    &change_directory,
+    &clear_screen,
+    &list_directory,
+    &print_environment,
+    &print_echo,
+    &show_help,
+    &pause_execution,
+    &exit_shell
+};
+
+// Функция для получения количества встроенных команд
+int get_num_commands() {
+    return sizeof(commands) / sizeof(char *);
 }
 
-// Функция для обработки команды cd
-void execute_cd(char *args[]) {
+// Функция для смены директории
+int change_directory(char **args, char *input_file, char *output_file, int append) {
     if (args[1] == NULL) {
-        // Если не указан каталог, выводим текущий каталог
-        printf("%s\n", getenv("PWD"));
-    } else if (chdir(args[1]) != 0) {
-        // Меняем текущий каталог и обрабатываем возможную ошибку
-        perror("myshell");
+        char *current_dir = getcwd(NULL, 0);
+        if (current_dir) {
+            printf("Current directory: %s\n", current_dir);
+            free(current_dir);
+        } else {
+            perror("Error");
+        }
     } else {
-        // Устанавливаем переменную среды PWD на новый каталог
-        setenv("PWD", args[1], 1);
+        if (chdir(args[1]) != 0) {
+            perror("Error");
+        } else {
+            setenv("PWD", args[1], 1);
+        }
     }
+    return 1;
 }
-// Функция для обработки команды dir
-void execute_dir(char *args[]) {
-    DIR *dir;
-    struct dirent *entry;
+
+// Функция для очистки экрана
+int clear_screen() {
+    printf("\033c"); // команда для очистки экрана терминала
+    return 1;
+}
+
+// Функция для вывода содержимого директории
+int list_directory(char **args, char *input_file, char *output_file, int append) {
     if (args[1] == NULL) {
-        // Если не указан каталог, используем текущий каталог
-        args[1] = ".";
-    }
-    if ((dir = opendir(args[1])) != NULL) {
-        // Считываем и выводим содержимое каталога
-        while ((entry = readdir(dir)) != NULL) {
-            printf("%s\n", entry->d_name);
-        }
-        closedir(dir);
+        fprintf(stderr, "Usage: dir <directory>\n");
     } else {
-        // Обрабатываем ошибку открытия каталога
-        perror("myshell");
-    }
-}
+        DIR *dir = opendir(args[1]);
+        if (dir == NULL) {
+            perror("Error opening directory");
+        } else {
+            FILE *output_stream = stdout;
+            if (output_file) {
+                output_stream = (append) ? fopen(output_file, "a") : fopen(output_file, "w");
+            }
 
-// Функция для обработки команды environ
-void execute_environ() {
-    // Выводим все переменные среды
-    for (char **env = environ; *env != 0; env++) {
-        char *thisEnv = *env;
-        printf("%s\n", thisEnv);
-    }
-}
-// Функция для обработки команды pause
-void execute_pause() {
-    // Приостанавливаем работу оболочки до нажатия Enter
-    printf("Press Enter to continue...\n");
-    while (getchar() != '\n');
-}
+            struct dirent *entry;
+            while ((entry = readdir(dir)) != NULL) {
+                fprintf(output_stream, "%s\n", entry->d_name);
+            }
 
-// Функция для обработки команды quit
-void execute_quit() {
-    // Завершаем работу оболочки
-    exit(0);
-}
-// Функция для обработки команды echo
-void execute_echo(char *args[]) {
-    // Выводим переданный комментарий
-    for (int i = 1; args[i] != NULL; i++) {
-        printf("%s ", args[i]);
-    }
-    printf("\n");
-}
-
-// Функция для обработки команды help
-void execute_help() {
-    printf("myshell - Простая реализация оболочки.\n");
-    printf("Поддерживаемые команды:\n");
-    printf("cd <directory> - Сменить каталог на <directory>.\n");
-    printf("clr - Очистить экран.\n");
-    printf("dir <directory> - Вывести содержимое каталога <directory>.\n");
-    printf("environ - Вывести все переменные среды.\n");
-    printf("echo <comment> - Вывести <comment> на экран.\n");
-    printf("help - Показать это сообщение с подсказками.\n");
-    printf("pause - Приостановить работу оболочки до нажатия Enter.\n");
-    printf("quit - Выйти из оболочки.\n");
-}
-
-// Функция для обработки команды clr
-void execute_clr() {
-    // Очищаем экран
-    printf("\033[H\033[J");
-}
-
-// Функция для выполнения внешней команды
-void execute_external_command(char *args[]) {
-    pid_t pid = fork();
-    if (pid == 0) {
-        // В дочернем процессе выполняем команду
-        execvp(args[0], args);
-        perror("myshell"); // Обрабатываем ошибку execvp
-        exit(EXIT_FAILURE);
-    } else if (pid < 0) {
-        // Обрабатываем ошибку fork
-        perror("myshell");
-    } else {
-        // В родительском процессе ждем завершения дочернего процесса
-        wait(NULL);
-    }
-}
-
-int main(int argc, char *argv[]){
-    char *line = NULL;
-    size_t len = 0;
-    ssize_t read;
-    char *args[100];
-    // Установить переменную среды shell
-    setenv("shell", argv[0], 1);
-
-    while (1) {
-        print_prompt(); // Выводим приглашение
-
-        read = getline(&line, &len, stdin); // Считываем строку ввода
-        if (read == -1) {
-            break;
+            closedir(dir);
+            if (output_file) fclose(output_stream);
         }
-        // Убираем символ новой строки
-        line[strcspn(line, "\n")] = 0;
+    }
+    return 1;
+}
 
-        // Разделяем строку на аргументы
-        int i = 0;
-        args[i] = strtok(line, " ");
-        while (args[i] != NULL) {
-            args[++i] = strtok(NULL, " ");
-            if (args[0] == NULL) {
-            continue; // Если команда пустая, продолжаем цикл
-            } else if (strcmp(args[0], "cd") == 0) {
-                execute_cd(args); // Выполняем команду cd
-            } else if (strcmp(args[0], "clr") == 0) {
-                execute_clr(); // Выполняем команду clr
-            } else if (strcmp(args[0], "dir") == 0) {
-                execute_dir(args); // Выполняем команду dir
-            } else if (strcmp(args[0], "environ") == 0) {
-                execute_environ(); // Выполняем команду environ
-            } else if (strcmp(args[0], "echo") == 0) {
-                execute_echo(args); // Выполняем команду echo
-            } else if (strcmp(args[0], "help") == 0) {
-                execute_help(); // Выполняем команду help
-            } else if (strcmp(args[0], "pause") == 0) {
-                execute_pause(); // Выполняем команду pause
-            } else if (strcmp(args[0], "quit") == 0) {
-                execute_quit(); // Выполняем команду quit
-            } else {
-                execute_external_command(args); // Выполняем внешнюю команду
+// Функция для вывода всех переменных окружения
+int print_environment(char **args, char *input_file, char *output_file, int append) {
+    FILE *output_stream = stdout;
+    if (output_file) {
+        output_stream = (append) ? fopen(output_file, "a") : fopen(output_file, "w");
+    }
+
+    char **env = environ;
+    while (*env != NULL) {
+        fprintf(output_stream, "%s\n", *env);
+        env++;
+    }
+
+    if (output_file) fclose(output_stream);
+    return 1;
+}
+
+// Функция для вывода строки на экран
+int print_echo(char **args, char *input_file, char *output_file, int append) {
+    FILE *output_stream = stdout;
+    if (output_file) {
+        output_stream = (append) ? fopen(output_file, "a") : fopen(output_file, "w");
+    }
+
+    if (args[1] == NULL) {
+        fprintf(output_stream, "\n");
+    } else {
+        for (int i = 1; args[i] != NULL; i++) {
+            fprintf(output_stream, "%s", args[i]);
+            if (args[i + 1] != NULL) {
+                fprintf(output_stream, " ");
             }
         }
+        fprintf(output_stream, "\n");
     }
+
+    if (output_file) fclose(output_stream);
+    return 1;
+}
+
+// Функция для вывода справки
+int show_help(char **args, char *input_file, char *output_file, int append) {
+    FILE *output_stream = stdout;
+    if (output_file) {
+        output_stream = (append) ? fopen(output_file, "a") : fopen(output_file, "w");
+    }
+
+    fprintf(output_stream, "List of available commands:\n");
+    for (int i = 0; i < get_num_commands(); i++) {
+        fprintf(output_stream, " %s\n", command_descriptions[i]);
+    }
+
+    if (output_file) fclose(output_stream);
+    return 1;
+}
+
+// Функция для паузы выполнения
+int pause_execution() {
+    printf("Press <Enter> to continue...");
+    while (getchar() != '\n');
+    return 1;
+}
+
+// Функция для выхода из оболочки
+int exit_shell() {
+    return 0;
+}
+
+// Функция для запуска внешней программы
+int launch_process(char **args, int background, char *input_file, char *output_file, int append) {
+    pid_t pid, wpid;
+    int status;
+    pid = fork();
+
+    if (pid == 0) { // дочерний процесс
+        if (input_file) {
+            freopen(input_file, "r", stdin);
+        }
+        if (output_file) {
+            if (append) {
+                freopen(output_file, "a", stdout);
+            } else {
+                freopen(output_file, "w", stdout);
+            }
+        }
+
+        if (execvp(args[0], args) == -1) {
+            perror("Error");
+        }
+        exit(EXIT_FAILURE);
+    } else if (pid < 0) {
+        perror("Error");
+    } else {
+        if (!background) {
+            do {
+                wpid = waitpid(pid, &status, WUNTRACED);
+            } while (!WIFEXITED(status) && !WIFSIGNALED(status));
+        }
+    }
+    return 1;
+}
+
+// Функция для выполнения команд
+int execute_command(char **args) {
+    if (args[0] == NULL) {
+        return 1;
+    }
+
+    char *input_file = NULL;
+    char *output_file = NULL;
+    int append = 0;
+
+    for (int i = 0; args[i] != NULL; i++) {
+        if (strcmp(args[i], "<") == 0) {
+            input_file = args[i + 1];
+            args[i] = NULL;
+        } else if (strcmp(args[i], ">") == 0) {
+            output_file = args[i + 1];
+            args[i] = NULL;
+        } else if (strcmp(args[i], ">>") == 0) {
+            output_file = args[i + 1];
+            args[i] = NULL;
+            append = 1;
+        }
+    }
+
+    int background = 0;
+    int last_arg_index = -1;
+    for (int i = 0; args[i] != NULL; i++) {
+        last_arg_index = i;
+    }
+    if (last_arg_index >= 0 && strcmp(args[last_arg_index], "&") == 0) {
+        background = 1;
+        args[last_arg_index] = NULL;
+    }
+
+    for (int i = 0; i < get_num_commands(); i++) {
+        if (strcmp(args[0], commands[i]) == 0) {
+            return (*command_functions[i])(args, input_file, output_file, append);
+        }
+    }
+
+    return launch_process(args, background, input_file, output_file, append);
+}
+
+// Функция для разбиения строки на аргументы
+char **split_line(char *line) {
+    int bufsize = LSH_TOK_BUFSIZE;
+    int position = 0;
+    char **tokens = malloc(bufsize * sizeof(char *));
+    char *token;
+
+    if (!tokens) {
+        fprintf(stderr, "Memory allocation error\n");
+        exit(EXIT_FAILURE);
+    }
+
+    token = strtok(line, LSH_TOK_DELIM);
+    while (token != NULL) {
+        tokens[position] = token;
+        position++;
+
+        if (position >= bufsize) {
+            bufsize += LSH_TOK_BUFSIZE;
+            tokens = realloc(tokens, bufsize * sizeof(char *));
+            if (!tokens) {
+                fprintf(stderr, "Memory allocation error\n");
+                exit(EXIT_FAILURE);
+            }
+        }
+
+        token = strtok(NULL, LSH_TOK_DELIM);
+    }
+    tokens[position] = NULL;
+    return tokens;
+}
+
+// Функция для чтения строки
+char *read_line() {
+    char *line = NULL;
+    ssize_t bufsize = 0;
+    getline(&line, &bufsize, stdin);
+    return line;
+}
+
+// Функция для выполнения оболочки
+void shell_loop() {
+    char *line;
+    char **args;
+    int status;
+    char path[256];
+
+    do {
+        getcwd(path, 256);
+        printf("(Shell) [%s] $ ", path);
+        line = read_line();
+        args = split_line(line);
+        status = execute_command(args);
+
+        free(line);
+        free(args);
+    } while (status);
+}
+
+int main() {
+    shell_loop();
+    return 0;
 }
--- myshell.h
+++ myshell.h
@@ -1,15 +1,37 @@
 #ifndef MYSHELL_H
 #define MYSHELL_H
 
-void print_prompt();
-void execute_cd(char *args[]);
-void execute_clr();
-void execute_dir(char *args[]);
-void execute_environ();
-void execute_echo(char *args[]);
-void execute_help();
-void execute_pause();
-void execute_quit();
-void execute_external_command(char *args[]);
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <dirent.h>
 
-#endif
+// Максимальная длина строки для ввода
+#define LSH_TOK_BUFSIZE 64
+#define LSH_TOK_DELIM " \t\r\n\a"
+
+// Прототипы функций
+int change_directory(char **args, char *input_file, char *output_file, int append);
+int clear_screen();
+int list_directory(char **args, char *input_file, char *output_file, int append);
+int print_environment(char **args, char *input_file, char *output_file, int append);
+int print_echo(char **args, char *input_file, char *output_file, int append);
+int show_help(char **args, char *input_file, char *output_file, int append);
+int pause_execution();
+int exit_shell();
+int launch_process(char **args, int background, char *input_file, char *output_file, int append);
+int execute_command(char **args);
+char **split_line(char *line);
+char *read_line();
+void shell_loop();
+int get_num_commands();
+
+extern char *commands[];
+extern char *command_descriptions[];
+extern int (*command_functions[])(char **, char *, char *, int);
+extern char **environ;
+
+#endif // MYSHELL_H
